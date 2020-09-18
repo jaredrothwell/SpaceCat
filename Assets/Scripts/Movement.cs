0301e04408566d509c1703be653a2be99022a528
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-18 08:00:26 -0600

    floating cat

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     [SerializeField] private float spd = 1.0f;
 
