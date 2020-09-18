32bf731d02b0e5789439e5c8a0a857001fc393e0
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-18 07:19:18 -0600

    stuff

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    private float Timer = 3f;
+    private float currentTimer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlaySound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentTimer += Time.deltaTime;
+        if(currentTimer >= Timer)
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/New Event", gameObject);
+            currentTimer = 0.0f;
+        }
     }
 }
