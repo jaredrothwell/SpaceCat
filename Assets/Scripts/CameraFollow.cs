0913793b4ecdbf18d3f0dbff22d81f3bb3a31407
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-18 08:37:25 -0600

    other objects can have gravity

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = gameObject.transform.position.z;
+        gameObject.transform.position = player.transform.position;
+        gameObject.transform.position += new Vector3(0, 0, z);
+
+    }
+}
