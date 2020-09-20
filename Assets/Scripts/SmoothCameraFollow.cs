856924c4ae67f307d916ad990006f27aec69545c
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-20 14:48:04 -0600

    fixed tile tearing

---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            float z = gameObject.transform.position.z;
+            gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, z);
+            gameObject.transform.position = new Vector3(RoundToNearestPixel(gameObject.transform.position.x, gameObject.GetComponent<Camera>()), RoundToNearestPixel(gameObject.transform.position.y, gameObject.GetComponent<Camera>()), z);
+        }
+
+    }
+
+    public static float RoundToNearestPixel(float unityUnits, Camera viewingCamera)
+    {
+        float valueInPixels = (Screen.height / (viewingCamera.orthographicSize * 2)) * unityUnits;
+        valueInPixels = Mathf.Round(valueInPixels);
+        float adjustedUnityUnits = valueInPixels / (Screen.height / (viewingCamera.orthographicSize * 2));
+        return adjustedUnityUnits;
+    }
+}
